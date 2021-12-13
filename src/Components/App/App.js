849bd8476d138d134a28a49166d92fc863ebb55a
maddyrandle <madisonrandle25@gmail.com>
2021-12-13 00:47:42 -0600

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>CTD Application Assignment</header>
+    </div>
+  );
+}
+
+export default App;
